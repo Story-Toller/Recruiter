eaebb67f50ee7df53ffcae588fa182c722042506
story <1269355513@qq.com>
2021-10-25 11:50:28 +0800

    招聘者项目初步搭建

--- api/src/test/java/com/yun/ApiApplicationTests.java
+++ api/src/test/java/com/yun/ApiApplicationTests.java
@@ -1,6 +1,11 @@
 package com.yun;
 
+import com.yun.beans.entity.Company;
 import com.yun.beans.entity.JobVo;
+import com.yun.beans.vo.JobCollectionVo;
+import com.yun.beans.vo.JobDesVo;
+import com.yun.dao.mapper.CompanyMapper;
+import com.yun.dao.mapper.CustomerCollectionMapper;
 import com.yun.dao.mapper.JobMapper;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
@@ -15,77 +20,104 @@ class ApiApplicationTests {
 
     @Autowired
     JobMapper jobMapper;
+    @Autowired
+    CompanyMapper companyMapper;
+    @Autowired
+    CustomerCollectionMapper customerCollectionMapper;
 
 
     @Test
     void contextLoads() {
 
         List<JobVo> showjobs = jobMapper.indexShowJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
 
     }
+
     @Test
     void contextLoads1() {
 
         List<JobVo> showjobs = jobMapper.indexFullJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
 
     }
+
     @Test
     void contextLoads2() {
 
         List<JobVo> showjobs = jobMapper.indexPartJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
 
     }
+
     @Test
     void contextLoads3() {
 
         List<JobVo> showjobs = jobMapper.showTechnicalJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
 
     }
+
     @Test
     void contextLoads4() {
 
         List<JobVo> showjobs = jobMapper.showAdministrativeJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
     }
+
     @Test
     void contextLoads5() {
 
         List<JobVo> showjobs = jobMapper.showDealJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
-    }@Test
+    }
+
+    @Test
     void contextLoads6() {
 
         List<JobVo> showjobs = jobMapper.showSaleJob();
-        for (JobVo j:showjobs){
+        for (JobVo j : showjobs) {
             System.out.println(j);
         }
-    }@Test
+    }
+
+    @Test
     void contextLoads7() {
+        List<JobDesVo> jobDesVos = jobMapper.showJobDes(5);
+        for (JobDesVo p : jobDesVos) {
+            System.out.println(p);
+        }
+    }
 
-        List<JobVo> showjobs = jobMapper.showMoneyJob();
-        for (JobVo j:showjobs){
-            System.out.println(j);
+    @Test
+    void contextLoads8() {
+        List<Company> companies = companyMapper.showAllCompanyInfo(2);
+        for (Company p : companies) {
+            System.out.println(p);
+        }
+    }
+
+    @Test
+    void contextLoads9() {
+        List<JobCollectionVo> jobCollectionVos = customerCollectionMapper.showJobCollection(24);
+        for (JobCollectionVo p : jobCollectionVos) {
+            System.out.println(p);
         }
     }
 
 
-
-    }
+}
 
 
